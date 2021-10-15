239e1aa18e9e906516bae6811b8279d112c2989f
Ben Schlegel <ben5.schlegel@gmail.com>
2021-10-15 17:07:48 +0200

    Add basic svg circle to overview, add reanimated babel plugin

--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -1,18 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Dimensions } from 'react-native';
 import { Routes, StackNavigationProps } from '../Routes';
+import Svg, { Circle } from 'react-native-svg';
+import Animated, { useSharedValue } from 'react-native-reanimated';
+
+const { width, height } = Dimensions.get('window');
+const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 
 const BACKGROUND_COLOR = '#444B6F'; //old: '#2E2E47'
 const BACKGROUND_COLOR_STROKE = '#303858';
 const STROKE_COLOR = '#A6E1FA';
+const STROKE_WIDTH = 30;
+const STROKE_WIDTH_INNER = 15;
+
+const CIRCLE_LENGTH = 850; //2PI*R
+const CIRCLE_RADIUS = CIRCLE_LENGTH / (2 * Math.PI);
 
 const Dashboard = ({ navigation }: StackNavigationProps<Routes, 'Dashboard'>): React.ReactElement => {
+  const progress = useSharedValue(0);
+
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
-      <Text>Dashboard</Text>
-      <Text>Dashboard</Text>
+      <Svg>
+        <Circle cx={width / 2} cy={height / 2} r={CIRCLE_RADIUS} stroke={BACKGROUND_COLOR_STROKE} strokeWidth={STROKE_WIDTH} />
+        <AnimatedCircle
+          cx={width / 2}
+          cy={height / 2}
+          r={CIRCLE_RADIUS}
+          stroke={STROKE_COLOR}
+          strokeWidth={STROKE_WIDTH_INNER}
+          strokeDasharray={CIRCLE_LENGTH}
+          strokeDashoffset={CIRCLE_LENGTH * 0.2}
+        />
+      </Svg>
     </View>
   );
 };
