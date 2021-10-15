ee4075f4266a8e418c0eb4185bb5aaffaa628f69
Ben Schlegel <ben5.schlegel@gmail.com>
2021-10-15 17:34:43 +0200

    Add animated progress bar

--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -1,9 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, View, Text, Dimensions } from 'react-native';
 import { Routes, StackNavigationProps } from '../Routes';
 import Svg, { Circle } from 'react-native-svg';
-import Animated, { useSharedValue } from 'react-native-reanimated';
+import Animated, { useAnimatedProps, useSharedValue, withTiming } from 'react-native-reanimated';
 
 const { width, height } = Dimensions.get('window');
 const AnimatedCircle = Animated.createAnimatedComponent(Circle);
@@ -20,9 +20,18 @@ const CIRCLE_RADIUS = CIRCLE_LENGTH / (2 * Math.PI);
 const Dashboard = ({ navigation }: StackNavigationProps<Routes, 'Dashboard'>): React.ReactElement => {
   const progress = useSharedValue(0);
 
+  useEffect(() => {
+    progress.value = withTiming(0.5, { duration: 1500 });
+  }, [progress]);
+
+  const animatedCircleProps = useAnimatedProps(() => ({
+    strokeDashoffset: CIRCLE_LENGTH * (1 - progress.value),
+  }));
+
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
+      <Text style={styles.balanceText}>3312.53$</Text>
       <Svg>
         <Circle cx={width / 2} cy={height / 2} r={CIRCLE_RADIUS} stroke={BACKGROUND_COLOR_STROKE} strokeWidth={STROKE_WIDTH} />
         <AnimatedCircle
@@ -32,7 +41,9 @@ const Dashboard = ({ navigation }: StackNavigationProps<Routes, 'Dashboard'>): R
           stroke={STROKE_COLOR}
           strokeWidth={STROKE_WIDTH_INNER}
           strokeDasharray={CIRCLE_LENGTH}
-          strokeDashoffset={CIRCLE_LENGTH * 0.2}
+          strokeOpacity={0.9}
+          animatedProps={animatedCircleProps}
+          strokeLinecap={'round'}
         />
       </Svg>
     </View>
@@ -45,6 +56,13 @@ const styles = StyleSheet.create({
     backgroundColor: BACKGROUND_COLOR,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  balanceText: {
+    color: '#FFF',
+    fontSize: 28,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
   },
 });
 
