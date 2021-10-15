c3912fc27c1059037a0565e4e75dccdbf5a8048d
Ben Schlegel <ben5.schlegel@gmail.com>
2021-10-15 16:55:36 +0200

    Fix navbar color in app.json, add color consts, add svg dependencies

--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -1,10 +1,17 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { Routes, StackNavigationProps } from '../Routes';
 
+const BACKGROUND_COLOR = '#444B6F'; //old: '#2E2E47'
+const BACKGROUND_COLOR_STROKE = '#303858';
+const STROKE_COLOR = '#A6E1FA';
+
 const Dashboard = ({ navigation }: StackNavigationProps<Routes, 'Dashboard'>): React.ReactElement => {
   return (
     <View style={styles.container}>
+      <StatusBar style="light" />
+      <Text>Dashboard</Text>
       <Text>Dashboard</Text>
     </View>
   );
@@ -13,7 +20,7 @@ const Dashboard = ({ navigation }: StackNavigationProps<Routes, 'Dashboard'>): R
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#2E2E47',
+    backgroundColor: BACKGROUND_COLOR,
     justifyContent: 'center',
     alignItems: 'center',
   },
