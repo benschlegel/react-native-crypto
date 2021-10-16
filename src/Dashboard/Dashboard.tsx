77f03673ac46e8650b7880c55fdb54c092d85242
Ben Schlegel <ben5.schlegel@gmail.com>
2021-10-16 10:38:13 +0200

    Add randomizer to chart

--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -1,19 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, Dimensions } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { StyleSheet, View, Dimensions, Text } from 'react-native';
 import { Routes, StackNavigationProps } from '../Routes';
 import { useDerivedValue, useSharedValue, withTiming, Easing } from 'react-native-reanimated';
 import { ReText } from 'react-native-redash';
 import { BACKGROUND_COLOR } from '../Constants/Constants';
-import { VictoryBar, VictoryChart, VictoryPie, VictoryTheme } from 'victory-native';
-const { height } = Dimensions.get('window');
+import { VictoryPie, VictoryTheme } from 'victory-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+// const { width, height } = Dimensions.get('window');
 
 const BALANCE_DURATION = 1500;
 
 const BALANCE = 3312.73;
 
-const wantedGraphicData = [{ y: 10 }, { y: 50 }, { y: 40 }]; // Data that we want to display
-const defaultGraphicData = [{ y: 0 }, { y: 0 }, { y: 100 }]; // Data used to make the animate prop work
+const wantedGraphicData = [{ y: 10 }, { y: 50 }, { y: 40 }, { y: 40 }, { y: 40 }]; // Data that we want to display
+const defaultGraphicData = [{ y: 0 }, { y: 0 }, { y: 0 }, { y: 0 }, { y: 100 }]; // Data used to make the animate prop work
+
+function getRandomInt(min: number, max: number): number {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 const Dashboard = ({ navigation }: StackNavigationProps<Routes, 'Dashboard'>): React.ReactElement => {
   const progress = useSharedValue(0);
@@ -24,6 +31,14 @@ const Dashboard = ({ navigation }: StackNavigationProps<Routes, 'Dashboard'>): R
   useEffect(() => {
     setGraphicData(wantedGraphicData); // Setting the data that we want to display
   }, []);
+
+  const randomizeChart = useCallback(() => {
+    const temp = Array.from(new Array(9), (x) => {
+      return { y: getRandomInt(10, 200) };
+    });
+    setGraphicData(temp);
+  }, []);
+
   useEffect(() => {
     progress.value = withTiming(0.5, { duration: BALANCE_DURATION });
   }, [progress]);
@@ -48,8 +63,12 @@ const Dashboard = ({ navigation }: StackNavigationProps<Routes, 'Dashboard'>): R
         padAngle={2}
         animate={{
           easing: 'exp',
+          duration: 1500,
         }}
       />
+      <TouchableOpacity style={styles.randomizeContainer} onPress={randomizeChart}>
+        <Text style={styles.randomizeText}>Randomize</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -67,6 +86,19 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     justifyContent: 'center',
     textAlign: 'center',
+  },
+  randomizeContainer: {
+    marginTop: 20,
+    width: 200,
+    height: 45,
+    borderRadius: 26,
+    backgroundColor: 'turquoise',
+    opacity: 0.8,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  randomizeText: {
+    color: 'white',
   },
 });
 
