e00620b911e4b800df031fe2a43d5862fff2078f
Ben Schlegel <ben5.schlegel@gmail.com>
2021-10-18 20:07:00 +0200

    Add preview coin component

---
+++ src/CoinPrice/CoinPrice.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import { StackNavigationProps, DashboardRoutes } from '../DashboardRoutes';
+const CoinPrice = ({ navigation }: StackNavigationProps<DashboardRoutes, 'Details'>): React.ReactElement => {
+  return (
+    <View style={styles.container}>
+      <Text>CoinPrice</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'green',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default CoinPrice;
---
+++ src/CoinPrice/index.ts
@@ -0,0 +1,1 @@
+export { default as CoinPrice } from './CoinPrice';
