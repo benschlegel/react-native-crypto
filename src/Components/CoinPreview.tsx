87cd50706d9e92ecc560357c0a5b8cc0dd4b38fd
Ben Schlegel <ben5.schlegel@gmail.com>
2021-10-18 11:44:59 +0200

    Add layout to CoinPreview

--- src/Components/CoinPreview.tsx
+++ src/Components/CoinPreview.tsx
@@ -11,16 +11,26 @@ interface CoinPreviewProps {
 
 const CoinPreview = ({}: CoinPreviewProps): React.ReactElement => {
   return (
-    <TouchableOpacity style={styles.container}>
-      <View>
-        <Text>Test</Text>
+    <TouchableOpacity style={styles.button}>
+      <View style={styles.container}>
+        <View style={styles.imageContainer}>
+          <Text>Test</Text>
+        </View>
+        <View style={styles.nameContainer}>
+          <Text style={styles.textFullname}>Bitcoin</Text>
+          <Text style={styles.textAbbreviation}>BTC</Text>
+        </View>
+        <View style={styles.changeContainer}>
+          <Text style={styles.textDailyChange}>+3,65%</Text>
+          <Text style={styles.textValueTotal}>$0.76</Text>
+        </View>
       </View>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  button: {
     backgroundColor: '#FFF',
     //Shadows for ios
     shadowColor: 'rgba(0,0,0, .4)',
@@ -38,6 +48,40 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     marginBottom: 10,
     marginTop: 5,
+  },
+  container: {
+    flexDirection: 'row',
+  },
+  imageContainer: {
+    alignItems: 'flex-start',
+    alignSelf: 'flex-start',
+    justifyContent: 'center',
+    marginLeft: 10,
+  },
+  nameContainer: {
+    alignItems: 'flex-start',
+    alignSelf: 'flex-start',
+    justifyContent: 'center',
+    marginLeft: 10,
+    flex: 1,
+  },
+  changeContainer: {
+    alignItems: 'flex-end',
+    alignSelf: 'flex-end',
+    justifyContent: 'center',
+    marginRight: 10,
+  },
+  textFullname: {
+    textAlign: 'left',
+  },
+  textAbbreviation: {
+    textAlign: 'left',
+  },
+  textDailyChange: {
+    textAlign: 'right',
+  },
+  textValueTotal: {
+    textAlign: 'right',
   },
 });
 
