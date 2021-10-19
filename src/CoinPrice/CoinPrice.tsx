e7a210e72cd9329eb5e8340837fa0c1056e89d9a
Ben Schlegel <ben5.schlegel@gmail.com>
2021-10-19 09:50:42 +0200

    Add styling to details view

--- src/CoinPrice/CoinPrice.tsx
+++ src/CoinPrice/CoinPrice.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useState } from 'react';
 import { StyleSheet, View, Text, SafeAreaView, ScrollView, RefreshControl, Image } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { BACKGROUND_COLOR, currencyFormat, TINT_COLOR } from '../Constants/Constants';
+import { BACKGROUND_COLOR, currencyFormat, TEXT_COLOR, TEXT_COLOR_HIGHLIGHT, TINT_COLOR } from '../Constants/Constants';
 import { wait } from '../Dashboard/Dashboard';
 import { DashboardRoutes, StackNavigationProps } from '../DashboardRoutes';
 
@@ -11,6 +11,8 @@ const CoinPrice = ({ route }: StackNavigationProps<DashboardRoutes, 'CoinPrice'>
   const { image, abbreviation, course, changePercentage, fullname } = route.params?.coin;
   const [refreshing, setRefreshing] = useState(false);
   const insets = useSafeAreaInsets();
+  const color = changePercentage >= 0 ? 'green' : 'rgba(255, 85, 114, 0.95)';
+
   const onRefresh = useCallback(() => {
     setRefreshing(true);
     wait(1200).then(() => {
@@ -33,11 +35,11 @@ const CoinPrice = ({ route }: StackNavigationProps<DashboardRoutes, 'CoinPrice'>
         }>
         <View style={styles.header}>
           <View style={styles.info}>
-            <Text>{fullname}</Text>
-            <Text>{currencyFormat(course)}</Text>
-            <Text>{`${changePercentage >= 0 ? '+' : ''}${(course * (changePercentage / 100)).toFixed(2)}$ / ${changePercentage.toFixed(
+            <Text style={styles.fullnameText}>{fullname}</Text>
+            <Text style={styles.courseText}>{currencyFormat(course)}</Text>
+            <Text style={[{ color }, styles.changeText]}>{`${changePercentage >= 0 ? '+' : ''}${(course * (changePercentage / 100)).toFixed(
               2,
-            )}% (1D)`}</Text>
+            )}$ / ${changePercentage.toFixed(2)}% (1D)`}</Text>
           </View>
           <View style={styles.imageContainer}>
             <Image style={styles.icon} source={image} />
@@ -74,8 +76,27 @@ const styles = StyleSheet.create({
     marginRight: MARGIN_OUTER,
   },
   icon: {
-    width: 64,
-    height: 64,
+    width: 70,
+    height: 70,
+  },
+  changeText: {
+    fontSize: 17,
+    fontFamily: 'SF-Pro-Text-Regular',
+    opacity: 0.95,
+  },
+  courseText: {
+    fontFamily: 'SF-Pro-Text-Bold',
+    fontSize: 28,
+    marginBottom: 2,
+    color: TEXT_COLOR_HIGHLIGHT,
+  },
+  fullnameText: {
+    textAlign: 'left',
+    fontFamily: 'SF-Pro-Text-Semibold',
+    color: TEXT_COLOR,
+    fontSize: 18,
+    opacity: 0.95,
+    marginBottom: 4,
   },
 });
 
