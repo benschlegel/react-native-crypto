524dbd2b2485941028e5716c3ea61ee64e4b0cd5
Ben Schlegel <ben5.schlegel@gmail.com>
2021-10-19 10:14:31 +0200

    Add styling and card to CoinPrice

--- src/CoinPrice/CoinPrice.tsx
+++ src/CoinPrice/CoinPrice.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback, useState } from 'react';
 import { StyleSheet, View, Text, SafeAreaView, ScrollView, RefreshControl, Image } from 'react-native';
+import Animated, { useAnimatedStyle, withTiming } from 'react-native-reanimated';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { BACKGROUND_COLOR, currencyFormat, TEXT_COLOR, TEXT_COLOR_HIGHLIGHT, TINT_COLOR } from '../Constants/Constants';
 import { wait } from '../Dashboard/Dashboard';
@@ -17,9 +18,16 @@ const CoinPrice = ({ route }: StackNavigationProps<DashboardRoutes, 'CoinPrice'>
     setRefreshing(true);
     wait(1200).then(() => {
       setRefreshing(false);
-      console.log('refresh');
+      //add logic, callback needed for refreshcontrol to work
     });
   }, []);
+
+  const animatedOpacity = useAnimatedStyle(() => {
+    return {
+      opacity: refreshing ? withTiming(0.65) : withTiming(1),
+    };
+  }, [refreshing]);
+
   return (
     <SafeAreaView style={[styles.container, { paddingTop: insets.top }]}>
       <ScrollView
@@ -33,16 +41,27 @@ const CoinPrice = ({ route }: StackNavigationProps<DashboardRoutes, 'CoinPrice'>
             colors={[TINT_COLOR]}
           />
         }>
+        {/* Header */}
         <View style={styles.header}>
           <View style={styles.info}>
             <Text style={styles.fullnameText}>{fullname}</Text>
-            <Text style={styles.courseText}>{currencyFormat(course)}</Text>
-            <Text style={[{ color }, styles.changeText]}>{`${changePercentage >= 0 ? '+' : ''}${(course * (changePercentage / 100)).toFixed(
-              2,
-            )}$ / ${changePercentage.toFixed(2)}% (1D)`}</Text>
+            <Animated.View style={animatedOpacity}>
+              <Text style={styles.courseText}>{currencyFormat(course)}</Text>
+              <Text style={[{ color }, styles.changeText]}>{`${changePercentage >= 0 ? '+' : ''}${(
+                course *
+                (changePercentage / 100)
+              ).toFixed(2)}$ / ${changePercentage.toFixed(2)}% (1D)`}</Text>
+            </Animated.View>
           </View>
           <View style={styles.imageContainer}>
             <Image style={styles.icon} source={image} />
+          </View>
+        </View>
+
+        {/* Overview */}
+        <View style={styles.overviewCard}>
+          <View style={styles.overviewContainer}>
+            <Text style={styles.fullnameText}>You own</Text>
           </View>
         </View>
       </ScrollView>
@@ -74,6 +93,24 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'flex-end',
     marginRight: MARGIN_OUTER,
+  },
+  overviewCard: {
+    marginHorizontal: MARGIN_OUTER,
+    marginTop: 10,
+    backgroundColor: '#FFF',
+    //Shadows for ios
+    shadowColor: 'rgba(0,0,0, .4)',
+    shadowOffset: { height: 1, width: 1 },
+    shadowOpacity: 1,
+    shadowRadius: 1,
+    //Shadows for android
+    elevation: 0.45,
+    borderRadius: 20,
+    height: 200,
+  },
+  overviewContainer: {
+    marginHorizontal: MARGIN_OUTER,
+    marginVertical: MARGIN_OUTER / 2,
   },
   icon: {
     width: 70,
