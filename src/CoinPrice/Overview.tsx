0ff81dce9b562d7fd62c4156a6e4d3c510c7e75f
Ben Schlegel <ben5.schlegel@gmail.com>
2021-10-19 10:40:30 +0200

    Add return text to card

--- src/CoinPrice/Overview.tsx
+++ src/CoinPrice/Overview.tsx
@@ -14,10 +14,17 @@ const Overview = ({ abbreviation }: OverviewProps): React.ReactElement => {
       <View style={styles.overviewContainer}>
         <Text style={styles.headerText}>You own</Text>
 
-        <Text style={[styles.courseText, { marginTop: 10 }]}>$236.15</Text>
+        <Text style={styles.courseText}>$236.15</Text>
+        <Text style={styles.coinText}>{`${abbreviation} 0.003871`}</Text>
         <View style={styles.overviewDetailsContainer}>
-          <Text style={styles.smallHeaderText}>Total return</Text>
-          <Text style={styles.smallHeaderText}>24h return</Text>
+          <View style={styles.returnContainer}>
+            <Text style={styles.smallHeaderText}>Total return</Text>
+            <Text style={styles.pricesText}>+$24.30</Text>
+            <View style={{ marginTop: 14 }}>
+              <Text style={styles.smallHeaderText}>24h return</Text>
+              <Text style={styles.pricesText}>+$3.24</Text>
+            </View>
+          </View>
         </View>
       </View>
     </View>
@@ -37,7 +44,7 @@ const styles = StyleSheet.create({
     //Shadows for android
     elevation: 0.45,
     borderRadius: 20,
-    height: 200,
+    // height: 200,
   },
   overviewContainer: {
     marginHorizontal: MARGIN_OUTER,
@@ -49,21 +56,36 @@ const styles = StyleSheet.create({
     color: TEXT_COLOR,
     fontSize: 16,
     opacity: 0.9,
-    marginBottom: 4,
+    marginBottom: 2,
   },
   courseText: {
     fontFamily: 'SF-Pro-Text-Bold',
     fontSize: 28,
-    marginBottom: 2,
+    marginTop: 10,
     color: TEXT_COLOR_HIGHLIGHT,
   },
+  coinText: {
+    color: 'gray',
+    fontFamily: 'SF-Pro-Text-Regular',
+    fontSize: 20,
+  },
   overviewDetailsContainer: {
-    marginTop: 10,
+    flexDirection: 'row',
+    marginTop: 20,
   },
   smallHeaderText: {
     textAlign: 'left',
     fontFamily: 'SF-Pro-Text-Regular',
+    color: 'gray',
     fontSize: 14,
+  },
+  returnContainer: {
+    marginRight: 60,
+  },
+  pricesText: {
+    fontFamily: 'SF-Pro-Text-Semibold',
+    fontSize: 18,
+    color: 'green',
     opacity: 0.9,
   },
 });
